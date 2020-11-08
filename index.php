002e6700c9d6eabe2327b494a059e129fce40b6b
Christopher E. Slowley <christopherslowley@gmail.com>
2020-11-07 20:32:43 -0800

    * Added style using sass generated css external files. * Made minor changes to the index.php and segment.header.php

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <html>
     
     <head>
+        <title>Php Test 1 - Home Page</title>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="js/getHeader.js"></script>
-        
+        <link rel="stylesheet" href="css/mainstyle.css"/>
+        <link rel="stylesheet" href="css/Tables.css"/>
     </head>
     <body>
         <div id="headerRow"></div>
-        <button onclick="getHeader()">Get Header</button>
+        <button onclick="getHeader()">Get Header using AJAX</button>
         <?php
             echo "Welcome to the World of PHP Web Development using the LAMP stack!" ;  
         ?>
+        <table class="normal-table">
+            
+        </table>
     </body>
 </html>
 
--- php/segment.header.php
+++ php/segment.header.php
@@ -1,4 +1,5 @@
 <?php
 $name = $_POST["name"];
+error_log("Inside function".__FILE__.":".__FUNCTION__);
 echo "Hello " . $name. "[".__FILE__."] ";
 
